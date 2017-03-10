96ef929f005749293bd604dc11ce504d8c786d95
Rebecca Morgan <beccamorgan92@gmail.com>
2017-03-10 15:56:40 +0000

    Initial commit

---
+++ due.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""
+due.py
+Python 2 script for todo.txt add-on
+Created by Rebecca Morgan 2017-03-10
+Copyright (c) 2017 Rebecca Morgan. All rights reserved.
+"""
+
+import os
+import sys
+from datetime import datetime
+import re
+
+def main(todo_dir):
+    # Prepare lists to store tasks
+    overdue = list()
+    due_today = list()
+
+    # Open todo.txt file
+    with open(os.path.join(todo_dir,'todo.txt'), 'r') as f:
+        content = f.readlines()
+        date = datetime.today()
+
+        # Loop through content and look for due dates, assuming the key due: is used and standard date format
+        for i, task in enumerate(content):
+            match = re.search(r'due:\d{4}-\d{2}-\d{2}', task)
+
+            if match is not None:
+                date = datetime.strptime(match.group()[4:], '%Y-%m-%d').date()
+
+                # Add matching tasks to list with line number
+                if date < datetime.today().date():
+                    overdue.append(str(i+1) + " " + task)
+                elif date == datetime.today().date():
+                    due_today.append(str(i+1) + " " + task)
+
+    # Print to console
+    if len(overdue) > 0:
+        print "================="
+        print "Overdue tasks:"
+        print "================="
+        for task in overdue:
+            print task,
+    if len(due_today) > 0:
+        print "================="
+        print "Tasks due today:"
+        print "================="
+        for task in due_today:
+            print task,
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "Usage: due.py [TODO_DIR]"
+        sys.exit(1)
+
+    if os.path.isdir(sys.argv[1]):
+        main(sys.argv[1])
+    else:
+        print "Error: %s is not a directory" % sys.argv[1]
+        sys.exit(1)
